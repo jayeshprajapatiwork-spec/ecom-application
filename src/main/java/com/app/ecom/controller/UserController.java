d77e31bf31094c3159ecb2d44254f0b00dadefe3
Jayesh Prajapati <jayeshprajapati.work@gmail.com>
2026-09-05 00:43:32 +0530

    added jpa

--- src/main/java/com/app/ecom/controller/UserController.java
+++ src/main/java/com/app/ecom/controller/UserController.java
@@ -1,14 +1,12 @@
-package com.app.ecom;
+package com.app.ecom.controller;
 
+import com.app.ecom.entity.User;
+import com.app.ecom.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
